219019459a932c42ad112fce96e89f5156ceecad
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2024-11-09 16:38:08 +0900

    chore: remove next-theme (until hydration error resolve)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import PageHeader from "@/components/blocks/page-header";
-import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ReactNode } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,12 +16,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={cn(inter.className)}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-          <PageHeader />
-          <main className="container mx-auto flex flex-col items-center justify-center py-4">
-            {children}
-          </main>
-        </ThemeProvider>
+        <PageHeader />
+        <main className="container mx-auto flex flex-col items-center justify-center py-4">
+          {children}
+        </main>
       </body>
     </html>
   );
