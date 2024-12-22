64ad29f24ab05a5aaa0f511fa88aeffed3c4518f
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2024-12-22 09:16:53 +0900

    feat: opengraph, manifest, robots

---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,57 @@
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+export const contentType = "image/png";
+
+export const alt = "東京ディズニーリゾートポータル";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export default async function Image() {
+  const notosansJP = fetch(
+    new URL("./NotoSansJP-Regular.ttf", import.meta.url),
+  ).then((res) => res.arrayBuffer());
+
+  return new ImageResponse(
+    <div
+      style={{
+        fontSize: 68,
+        color: "#fff1f2",
+        background: "#e11d48",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+        paddingLeft: 50,
+        justifyContent: "center",
+      }}
+    >
+      🏰 東京ディズニーリゾートポータル
+      <div
+        style={{
+          display: "flex",
+          fontSize: 24,
+        }}
+      >
+        <p>
+          東京ディズニーリゾートの待ち時間を便利に簡単に確認できるポータルサイトです
+          ✨
+        </p>
+      </div>
+    </div>,
+    {
+      ...size,
+      fonts: [
+        {
+          name: "NotoSansJP-Regular",
+          data: await notosansJP,
+          style: "normal",
+          weight: 400,
+        },
+      ],
+    },
+  );
+}
